638ad647d6563740c6fe6792140c705636e02078
AliaaHegazy <aliaahegazy2004@gmail.com>
2023-07-18 08:20:51 -0700

    alx

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,33 +1,14 @@
- 
+#include <unistd.h>
+#include "main.h"
 
- /*
- * File: 0-putchar.c 
+/**
+ * _putchar -writes the character c to stbout
+ * @c: the character to print
  *
+ * Return: on success 1.
+ *	on error , -1 is returned, and errno is set appropriately.
  */
-
- #include "main.h" 
-
- /** 
- * main-print putchar followed by a new line
- * 
- *Return :always 0
- */
-  int main(void) 
- {
-      _putchar('_' );
-      _putchar ('p' );
-      _putchar ('u' );
-      _putchar ('t' );
-      _putchar ('c' );
-      _putchar ('h' );
-      _putchar ('a' );
-      _putchar ('r' );
-      _putchar ('\n');
-      
-       return (0) ;
-} 
-        
-
-
-
-
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
